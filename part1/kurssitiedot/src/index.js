794da0738008f79aea34c8acb3b95193845a46d1
Aija <aija.valleala@gmail.com>
2020-12-31 15:43:30 +0200

    exercise 2.2 & 2.3

--- part1/kurssitiedot/src/index.js
+++ part1/kurssitiedot/src/index.js
@@ -2,46 +2,45 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
-const Header = (props) => {
-  return <h1>{props.course}</h1>
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
 }
 
-const Content = (props) => {
+const Header = ({ name }) => {
+  return <h1>{name}</h1>
+}
+
+const Content = ({ parts }) => {
   return (
     <>
-      {props.parts.map((part) => (
+      {parts.map((part) => (
         <Part key={part.id} part={part} />
       ))}
     </>
   )
 }
 
-const Part = (props) => {
+const Part = ({ part }) => {
   return (
     <p>
-      {props.part.name} {props.part.exercises}
+      {part.name} {part.exercises}
     </p>
   )
 }
 
-// const Total = (props) => {
-//   return (
-//     <p>
-//       Number of exercises{' '}
-//       {props.parts[0].exercises +
-//         props.parts[1].exercises +
-//         props.parts[2].exercises}
-//     </p>
-//   )
-// }
-
-const Course = ({ course }) => {
+const Total = ({ parts }) => {
+  const total = parts.map((part) => part.exercises).reduce((a, c) => a + c)
+  console.log(total)
   return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      {/* <Total parts={course.parts} /> */}
-    </div>
+    <p>
+      <strong>Total of {total} exercises</strong>
+    </p>
   )
 }
 
@@ -66,8 +65,8 @@ const App = () => {
         id: 3,
       },
       {
-        name: 'State of a component 2',
-        exercises: 3,
+        name: 'Redux',
+        exercises: 11,
         id: 4,
       },
     ],
