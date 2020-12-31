414f99a67a29da674e95e1025022f3425b44678b
Aija <aija.valleala@gmail.com>
2020-12-31 12:26:53 +0200

    exercise 1.6

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,17 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+)
+
+const Stat = ({ text, count }) => (
+  <p>
+    {text} {count}
+  </p>
+)
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button
+        handleClick={() => {
+          setGood(good + 1)
+        }}
+        text='good'
+      />
+      <Button
+        handleClick={() => {
+          setNeutral(neutral + 1)
+        }}
+        text='neutral'
+      />
+      <Button
+        handleClick={() => {
+          setBad(bad + 1)
+        }}
+        text='bad'
+      />
+      <h1>statistics</h1>
+      <Stat count={good} text='good' />
+      <Stat count={neutral} text='neutral' />
+      <Stat count={bad} text='bad' />
+    </div>
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
