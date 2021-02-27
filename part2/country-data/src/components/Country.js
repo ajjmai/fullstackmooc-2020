223236998f00fe9c62a17dd779a7e4cd41222eb7
Aija <aija.valleala@gmail.com>
2021-02-27 18:26:10 +0200

    exercise 2.14

--- part2/country-data/src/components/Country.js
+++ part2/country-data/src/components/Country.js
@@ -1,6 +1,41 @@
 import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+const api_key = process.env.REACT_APP_API_KEY
+
+const Weather = ({ city, weather }) => {
+  return (
+    <>
+      <h2>Weather in {city}</h2>
+      <div>
+        <img
+          alt='weather-icon'
+          src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+        />
+      </div>
+      <p>
+        <strong>temperature:</strong> {Math.round(weather.main.temp)} Celsius
+      </p>
+      <p>
+        <strong>wind: </strong> {weather.wind.speed} m/s
+      </p>
+    </>
+  )
+}
 
 const Country = ({ country }) => {
+  const [weather, setWeather] = useState({})
+
+  useEffect(() => {
+    const city = country.capital
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.toLowerCase()}&appid=${api_key}&units=metric`
+    // console.log(url)
+    axios.get(url).then((response) => {
+      // console.log(response.data)
+      setWeather(response.data)
+    })
+  }, [country.capital])
+
   return (
     <>
       <h1>{country.name}</h1>
@@ -13,6 +48,7 @@ const Country = ({ country }) => {
         ))}
       </ul>
       <img width='150px' src={country.flag} alt={`flag of ${country.name}`} />
+      {weather.name && <Weather city={country.capital} weather={weather} />}
     </>
   )
 }
