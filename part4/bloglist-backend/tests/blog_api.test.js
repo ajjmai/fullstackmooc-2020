728afeef9cc69ad17d49fa56f588a48666ec34d4
Aija Valleala <aija.valleala@gmail.com>
2021-05-02 19:52:21 +0300

    exercise 4.9

--- part4/bloglist-backend/tests/blog_api.test.js
+++ part4/bloglist-backend/tests/blog_api.test.js
@@ -29,6 +29,11 @@ test('a specific blog is within the returned blogs', async () => {
   expect(titles).toContain('React patterns')
 })
 
+test('blogs contain field "id"', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body[0].id).toBeDefined()
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
