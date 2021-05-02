bf76e7e85224736e3d0835c700be0d0aaac36f68
Aija Valleala <aija.valleala@gmail.com>
2021-05-02 20:15:52 +0300

    exercise 4.19

--- part4/bloglist-backend/controllers/blogs.js
+++ part4/bloglist-backend/controllers/blogs.js
@@ -1,6 +1,15 @@
+const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -10,7 +19,13 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
-  const user = await User.findById('608e526eab583116ccd00273')
+  const token = getTokenFrom(request)
+  const decocedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decocedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+
+  const user = await User.findById(decocedToken.id)
 
   const blog = new Blog({
     title: body.title,
