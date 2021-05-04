88c44844b000d2432dc9c7f2b8201fe82d3ef7bf
Aija Valleala <aija.valleala@gmail.com>
2021-05-04 19:33:42 +0300

    exercise 5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,38 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [expand, setExpand] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+
+  const handleExpand = () => {
+    setExpand(!expand)
+  }
+
+  const fullInfo = () => (
+    <>
+      <p>{blog.url}</p>
+      <p>
+        likes {blog.likes}
+        <button>Like</button>
+      </p>
+      <p>{blog.user.name}</p>
+    </>
+  )
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} by {blog.author}{' '}
+      <button onClick={handleExpand}>{expand ? 'hide' : 'view'}</button>
+      {expand && fullInfo()}
+    </div>
+  )
+}
 
 export default Blog
