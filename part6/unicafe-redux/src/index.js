3e0fd20ff2fe5f5fa06e059ab55fc43a8837be8f
Aija Valleala <aija.valleala@gmail.com>
2021-05-07 10:15:39 +0300

    exercise 6.2

--- part6/unicafe-redux/src/index.js
+++ part6/unicafe-redux/src/index.js
@@ -1,26 +1,47 @@
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
 import reducer from './reducer'
 
-const store = createStore(reducer)
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 const App = () => {
   const good = () => {
     store.dispatch({
-      type: 'GOOD'
+      type: 'GOOD',
+    })
+  }
+
+  const ok = () => {
+    store.dispatch({
+      type: 'OK',
+    })
+  }
+
+  const bad = () => {
+    store.dispatch({
+      type: 'BAD',
+    })
+  }
+
+  const reset = () => {
+    store.dispatch({
+      type: 'ZERO',
     })
   }
 
   return (
     <div>
-      <button onClick={good}>good</button> 
-      <button>neutral</button> 
-      <button>bad</button>
-      <button>reset stats</button>
+      <button onClick={good}>good</button>
+      <button onClick={ok}>neutral</button>
+      <button onClick={bad}>bad</button>
+      <button onClick={reset}>reset stats</button>
       <div>good {store.getState().good}</div>
-      <div>neutral</div>
-      <div>bad</div>
+      <div>neutral {store.getState().ok}</div>
+      <div>bad {store.getState().bad} </div>
     </div>
   )
 }
