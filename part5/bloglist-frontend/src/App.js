0ffa9110ecb36105bff2ebe52c57ecb08407961e
Aija Valleala <aija.valleala@gmail.com>
2021-05-13 19:00:22 +0300

    exercise 7.14

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,18 +1,22 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { Switch, Route, useRouteMatch } from 'react-router-dom'
 import { initialiseBlogs } from './reducers/blogReducer'
 import { initialiseUsers } from './reducers/userReducer'
 import { login, getUserFromToken } from './reducers/loginReducer'
 import Notification from './components/Notification'
 import LoginForm from './components/LoginForm'
 import UserList from './components/UserList'
+import User from './components/User'
 import BlogList from './components/BlogList'
 import LoggedUserInfo from './components/LoggedUserInfo'
 
 const App = () => {
   const dispatch = useDispatch()
-  const user = useSelector((state) => state.login)
+  const loggedInUser = useSelector((state) => state.login)
+  const users = useSelector((state) => state.users)
+
+  const match = useRouteMatch('/users/:id')
 
   useEffect(() => {
     dispatch(initialiseBlogs())
@@ -30,20 +34,25 @@ const App = () => {
     dispatch(login(userObject))
   }
 
+  const user = match ? users.find((user) => user.id === match.params.id) : null
+
   return (
-    <Router>
+    <>
       <>
         <h1>Bloglist</h1>
         <Notification />
-        {!user ? <LoginForm handleLogin={handleLogin} /> : <LoggedUserInfo />}
+        {!loggedInUser ? (
+          <LoginForm handleLogin={handleLogin} />
+        ) : (
+          <LoggedUserInfo />
+        )}
       </>
       <Switch>
-        <Route path='/users'>
-          <UserList />
-        </Route>
-        <Route path='/'>{user && <BlogList />}</Route>
+        <Route path='/users/:id'>{loggedInUser && <User user={user} />}</Route>
+        <Route path='/users'>{loggedInUser && <UserList />}</Route>
+        <Route path='/'>{loggedInUser && <BlogList />}</Route>
       </Switch>
-    </Router>
+    </>
   )
 }
 
--- part5/bloglist-frontend/src/components/LoggedUserInfo.js
+++ part5/bloglist-frontend/src/components/LoggedUserInfo.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { useHistory } from 'react-router-dom'
 import { setNotification } from '../reducers/notificationReducer'
 import { logout } from '../reducers/loginReducer'
 
 const LoggedUserInfo = () => {
   const dispatch = useDispatch()
   const user = useSelector((state) => state.login)
+  const history = useHistory()
 
   const handleLogout = (event) => {
     event.preventDefault()
     dispatch(logout())
     dispatch(setNotification('Logging out completed.', 'notification'))
+    history.push('/')
   }
 
   return (
