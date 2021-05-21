700be1c3527fbc69e94d01d66de2caf7e1ac689a
Aija Valleala <aija.valleala@gmail.com>
2021-05-21 18:45:22 +0300

    exercise 8.7

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -109,6 +109,7 @@ const typeDefs = gql`
 
   type Mutation {
     addBook(title: String!, author: String!, published: Int!, genres: [String]!): Book
+    editAuthor(name: String!, setBornTo: Int!): Author
   }
 `
 
@@ -146,6 +147,15 @@ const resolvers = {
       }
 
       return book
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find((a) => a.name === args.name)
+      if (!author) {
+        return null
+      }
+      const updatedAuthor = { ...author, born: args.setBornTo }
+      authors = authors.map((a) => (a.name === args.name ? updatedAuthor : a))
+      return updatedAuthor
     },
   },
 }
